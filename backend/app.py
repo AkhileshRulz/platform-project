a9d22d6c71634c89a5722385df90f6b3373b1075
Alex <alex@example.com>
2026-02-03 19:05:57 +0000

    Initial platform project

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import os
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Backend is running!"
+
+@app.route("/db")
+def db_test():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            database=os.environ.get("POSTGRES_DB"),
+            user=os.environ.get("POSTGRES_USER"),
+            password=os.environ.get("POSTGRES_PASSWORD")
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        version = cur.fetchone()
+        return f"DB Connected! PostgreSQL version: {version}"
+    except Exception as e:
+        return f"DB connection failed: {e}"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
